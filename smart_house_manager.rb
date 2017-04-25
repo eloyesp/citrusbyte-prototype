98b9b0808174796dbbe9f000b2bfebadbd2695cf
Eloy Espinaco <eloyesp@gmail.com>
2017-04-25 11:22:27 -0300

    Basic list of devises

--- smart_house_manager.rb
+++ smart_house_manager.rb
@@ -6,6 +6,35 @@ Cuba.plugin(Mote::Render)
 
 Cuba.define do
   on root do
-    render 'admin_dashboard'
+    render 'admin_dashboard', { devices: DEVICES }
   end
 end
+
+DEVICES = [
+  { name: 'Samsung Audio',
+    controls: [{
+      name: 'Power',
+      type: 'button'
+    }, {
+      name: 'Volume',
+      type: 'slider'
+    }, {
+      name: 'Playlist',
+      type: 'select',
+      options: [ 'Lonely Day', 'Soldier Side' ]
+    }]
+  }, {
+    name: 'Sony Audio',
+    controls: [{
+      name: 'Power',
+      type: 'button'
+    }, {
+      name: 'Volume',
+      type: 'slider'
+    }, {
+      name: 'Playlist',
+      type: 'select',
+      options: [ 'Lonely Day', 'Soldier Side' ]
+    }]
+  }
+]
